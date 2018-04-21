cc25b2c6a5bae65d5d427ae82776612338794d27
KrzysztofZer <krzysiekzerman@gmail.com>
2018-04-21 21:48:44 +0200

    Fixing after review

---
+++ Klient/venv/menu.py
@@ -0,0 +1,49 @@
+import Input_data
+import sys
+import save_created_data
+
+def menu():
+    x = list_of_activities()
+    if x == '1':
+        create_bug()
+        return
+    elif x == '2':
+        show_bugs()
+        return
+    elif x == '10':
+        exit_menu()
+        return
+    else:
+        number_out_of_range()
+    return
+
+
+def create_bug():
+    object_to_create = Input_data.InputData()
+    object_to_create.inputting()
+    save_created_data.send_data(object_to_create)
+    menu()
+
+
+def number_out_of_range():
+    print("****Pls provide number from list below!!!****")
+    menu()
+
+
+def show_bugs():
+    print("Bug 1")
+    print("Bug 2")
+    menu()
+
+
+def list_of_activities():
+    print("1.  Create new bug report")
+    print("2.  Show list of bugs")
+    print("10. Exit")
+    x = input("Input number of activity: ")
+    print('\n\n')
+    return x
+
+
+def exit_menu():
+    print("Program is ended")
